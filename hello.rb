dc719a010185816d5a6d8afa7d7c32a56c6b3072
Chyngyz Dyikanbaev <chipalino@live.com>
2015-02-18 10:10:36 +0600

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -9,6 +9,7 @@ def greet(variable)
 	=> "Hello, Azamat!"
 =end
 	# write your code here
+	"Hello, #{variable}!"
 end
 
 
@@ -24,4 +25,5 @@ def hello
 	=> "Hello!"
 =end
 	# write your code here
+	"Hello!"
 end
